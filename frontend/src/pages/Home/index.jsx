155ca7c549148c7a8e76784d54c91c5325049c70
ouharri <ouharrioutman@gmail.com>
2023-06-14 02:32:11 +0100

    update

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -128,6 +128,60 @@ const Home = () => {
             </div>
             <Company/>
             <People/>
+            <div className="bg-red-300">
+                <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center">
+                    <div className="lg:w-0 lg:flex-1">
+                        <h2 className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl"
+                            id="newsletter-headline">
+                            Sign up for our newsletter
+                        </h2>
+                        <p className="mt-3 max-w-3xl text-lg leading-6 text-gray-300">
+                            Welcome to FleetLinker, the ultimate fleet management solution for efficient and streamlined
+                            operations. Whether you're managing a small fleet or overseeing a large-scale transportation
+                            network, our web-based app empowers you with the tools and insights you need to optimize
+                            your fleet's performance.
+
+                            Get Started in 2 Easy Steps:
+
+                            1. Sign up: Create your FleetLinker account by providing your basic information. It only
+                            takes a few minutes to get started.
+
+                            2. Set up your fleet: Add your vehicles, drivers, and other relevant details to our
+                            user-friendly interface. Customize the app to suit your specific fleet management needs.
+                        </p>
+                    </div>
+                    <div className="mt-8 lg:mt-0 lg:ml-8">
+                        <form className="sm:flex">
+                            <label htmlFor="email-address" className="sr-only">
+                                Email address
+                            </label>
+                            <input
+                                id="email-address"
+                                name="email-address"
+                                type="email"
+                                autoComplete="email"
+                                required
+                                className="w-full px-5 py-3 border border-transparent placeholder-gray-500 focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white focus:border-white sm:max-w-xs rounded-md"
+                                placeholder="Enter your email"
+                            />
+                            <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
+                                <button
+                                    type="submit"
+                                    className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
+                                >
+                                    Notify me
+                                </button>
+                            </div>
+                        </form>
+                        <p className="mt-3 text-sm text-gray-300">
+                            We care about the protection of your data. Read our{' '}
+                            <a href="#" className="text-white font-medium underline">
+                                Privacy Policy.
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
             <Footer/>
         </>
     )
